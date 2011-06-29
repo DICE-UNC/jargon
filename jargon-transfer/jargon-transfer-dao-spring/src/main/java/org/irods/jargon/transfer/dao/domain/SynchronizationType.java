b416f3fb3382ed13079e9684530a9e97b47af6f9
Jason Reilly <jdr0887@renci.org>
2011-06-29 16:12:17 -0400

    initial commit...again

---
+++ jargon-transfer/jargon-transfer-dao-spring/src/main/java/org/irods/jargon/transfer/dao/domain/SynchronizationType.java
@@ -0,0 +1,17 @@
+package org.irods.jargon.transfer.dao.domain;
+
+/**
+ * Enumeration of synchronization modes.
+ * 
+ * @author Mike Conway - DICE (www.irods.org)
+ * 
+ */
+public enum SynchronizationType {
+
+    ONE_WAY_LOCAL_TO_IRODS,
+
+    ONE_WAY_IRODS_TO_LOCAL,
+
+    BI_DIRECTIONAL
+
+}
