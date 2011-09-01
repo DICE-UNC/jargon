5913c23aaa88e6b5bf7e5414cae0e8d9d32daac4
Mike Conway <mikeconway@dhcp152-54-6-70.wireless.europa.renci.org>
2011-09-01 09:11:04 -0400

    [#456] Transfer Pipeline Optimization - remove setters in PipelineConfiguration and synchs

--- jargon-core/src/main/java/org/irods/jargon/core/connection/PipelineConfiguration.java
+++ jargon-core/src/main/java/org/irods/jargon/core/connection/PipelineConfiguration.java
@@ -4,14 +4,15 @@
 package org.irods.jargon.core.connection;
 
 /**
- * Immutable object represents the options controlling the behavior of the io pipeline.  Typically, these options are built based on the current
- * state of the {@link JargonProperties} at the time a connection is created.
+ * Immutable object represents the options controlling the behavior of the io
+ * pipeline. Typically, these options are built based on the current state of
+ * the {@link JargonProperties} at the time a connection is created.
  * 
  * @author Mike Conway - DICE (www.irods.org)
- *
+ * 
  */
 public class PipelineConfiguration {
-	
+
 	private final int irodsSocketTimeout;
 	private final int irodsParallelSocketTimeout;
 	private int internalInputStreamBufferSize;
@@ -19,33 +20,42 @@ public class PipelineConfiguration {
 	private int internalCacheBufferSize;
 	private int sendInputStreamBufferSize;
 	private int localFileOutputStreamBufferSize;
-	
+
 	/**
-	 * Static initializer method will derive an immutable <code>PipelineConfiguration</code> based on the prevailing <code>JargonProperties</code> at the time the 
-	 * connection is created.
+	 * Static initializer method will derive an immutable
+	 * <code>PipelineConfiguration</code> based on the prevailing
+	 * <code>JargonProperties</code> at the time the connection is created.
+	 * 
 	 * @param jargonProperties
 	 * @return
 	 */
-	public static PipelineConfiguration instance(final JargonProperties jargonProperties) {
+	public static PipelineConfiguration instance(
+			final JargonProperties jargonProperties) {
 		return new PipelineConfiguration(jargonProperties);
 	}
 
 	private PipelineConfiguration(final JargonProperties jargonProperties) {
-		
+
 		if (jargonProperties == null) {
 			throw new IllegalArgumentException("null jargonProperties");
 		}
-		
+
 		this.irodsSocketTimeout = jargonProperties.getIRODSSocketTimeout();
-		this.irodsParallelSocketTimeout = jargonProperties.getIRODSParallelTransferSocketTimeout();
-		this.internalInputStreamBufferSize = jargonProperties.getInternalInputStreamBufferSize();
-		this.internalOutputStreamBufferSize = jargonProperties.getInternalOutputStreamBufferSize();
-		this.internalCacheBufferSize = jargonProperties.getInternalCacheBufferSize();
-		this.sendInputStreamBufferSize = jargonProperties.getSendInputStreamBufferSize();
-		this.localFileOutputStreamBufferSize = jargonProperties.getLocalFileOutputStreamBufferSize();
-		
+		this.irodsParallelSocketTimeout = jargonProperties
+				.getIRODSParallelTransferSocketTimeout();
+		this.internalInputStreamBufferSize = jargonProperties
+				.getInternalInputStreamBufferSize();
+		this.internalOutputStreamBufferSize = jargonProperties
+				.getInternalOutputStreamBufferSize();
+		this.internalCacheBufferSize = jargonProperties
+				.getInternalCacheBufferSize();
+		this.sendInputStreamBufferSize = jargonProperties
+				.getSendInputStreamBufferSize();
+		this.localFileOutputStreamBufferSize = jargonProperties
+				.getLocalFileOutputStreamBufferSize();
+
 	}
-	
+
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
@@ -68,31 +78,15 @@ public class PipelineConfiguration {
 	/**
 	 * @return the internalInputStreamBufferSize
 	 */
-	public synchronized int getInternalInputStreamBufferSize() {
+	public int getInternalInputStreamBufferSize() {
 		return internalInputStreamBufferSize;
-	}
-
-	/**
-	 * @param internalInputStreamBufferSize the internalInputStreamBufferSize to set
-	 */
-	public synchronized void setInternalInputStreamBufferSize(
-			int internalInputStreamBufferSize) {
-		this.internalInputStreamBufferSize = internalInputStreamBufferSize;
 	}
 
 	/**
 	 * @return the internalOutputStreamBufferSize
 	 */
-	public synchronized int getInternalOutputStreamBufferSize() {
+	public int getInternalOutputStreamBufferSize() {
 		return internalOutputStreamBufferSize;
-	}
-
-	/**
-	 * @param internalOutputStreamBufferSize the internalOutputStreamBufferSize to set
-	 */
-	public synchronized void setInternalOutputStreamBufferSize(
-			int internalOutputStreamBufferSize) {
-		this.internalOutputStreamBufferSize = internalOutputStreamBufferSize;
 	}
 
 	/**
@@ -102,13 +96,7 @@ public class PipelineConfiguration {
 		return internalCacheBufferSize;
 	}
 
-	/**
-	 * @param internalCacheBufferSize the internalCacheBufferSize to set
-	 */
-	public synchronized void setInternalCacheBufferSize(int internalCacheBufferSize) {
-		this.internalCacheBufferSize = internalCacheBufferSize;
-	}
-
+	
 	/**
 	 * @return the sendInputStreamBufferSize
 	 */
@@ -117,26 +105,10 @@ public class PipelineConfiguration {
 	}
 
 	/**
-	 * @param sendInputStreamBufferSize the sendInputStreamBufferSize to set
-	 */
-	public synchronized void setSendInputStreamBufferSize(
-			int sendInputStreamBufferSize) {
-		this.sendInputStreamBufferSize = sendInputStreamBufferSize;
-	}
-
-	/**
 	 * @return the localFileOutputStreamBufferSize
 	 */
 	public synchronized int getLocalFileOutputStreamBufferSize() {
 		return localFileOutputStreamBufferSize;
-	}
-
-	/**
-	 * @param localFileOutputStreamBufferSize the localFileOutputStreamBufferSize to set
-	 */
-	public synchronized void setLocalFileOutputStreamBufferSize(
-			int localFileOutputStreamBufferSize) {
-		this.localFileOutputStreamBufferSize = localFileOutputStreamBufferSize;
 	}
 
 	/**
@@ -152,5 +124,5 @@ public class PipelineConfiguration {
 	public synchronized int getIrodsParallelSocketTimeout() {
 		return irodsParallelSocketTimeout;
 	}
-	
+
 }
