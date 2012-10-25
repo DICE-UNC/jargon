3f31b0839de3d41a6006cac9f8f0400e80cf3dc5
mconway <michael.c.conway@gmail.com>
2012-10-25 11:53:40 -0400

    [#132] GSS and Kerberos Support in Jargon-core

--- jargon-core/src/main/java/org/irods/jargon/core/utils/JargonVersion.java
+++ jargon-core/src/main/java/org/irods/jargon/core/utils/JargonVersion.java
@@ -1,5 +1,5 @@
 package org.irods.jargon.core.utils;
 public final class JargonVersion {
  public static String VERSION="3.2.2-SNAPSHOT";
- public static String BUILD_TIME="20121018-1550";
+ public static String BUILD_TIME="20121025-1152";
 }
