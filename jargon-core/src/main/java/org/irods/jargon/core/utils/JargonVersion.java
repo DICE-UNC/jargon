d2c121d804eeab6c38eac19dff4385e08fcb50ba
mconway <michael.c.conway@gmail.com>
2012-10-30 12:56:48 -0400

    [#1039] invalid auth potentially leaving open connection/agent

--- jargon-core/src/main/java/org/irods/jargon/core/utils/JargonVersion.java
+++ jargon-core/src/main/java/org/irods/jargon/core/utils/JargonVersion.java
@@ -1,5 +1,5 @@
 package org.irods.jargon.core.utils;
 public final class JargonVersion {
  public static String VERSION="3.2.1-SNAPSHOT";
- public static String BUILD_TIME="20121030-1215";
+ public static String BUILD_TIME="20121030-1231";
 }
