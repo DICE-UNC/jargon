8eed74e5a810ce6526d809761cf8ce4db9b30f42
mconway <michael.c.conway@gmail.com>
2012-11-19 15:01:48 -0500

    [#984] additional user profile fields

--- jargon-user-profile/src/main/java/org/irods/jargon/userprofile/UserProfileConstants.java
+++ jargon-user-profile/src/main/java/org/irods/jargon/userprofile/UserProfileConstants.java
@@ -15,7 +15,19 @@ public class UserProfileConstants {
 	public static final String USER_NAME = "userName";
 	public static final String DESCRIPTION = "description";
 	public static final String NICK_NAME = "nickName";
-
+	public static final String CN = "cn";
+	public static final String GIVEN_NAME = "givenName";
+	public static final String SN = "sn";
+	public static final String POSTAL_ADDRESS = "postalAddress";
+	public static final String POSTAL_CODE = "postalCode";
+	public static final String POST_OFFICE_BOX = "postOfficeBox";
+	public static final String LOCALITY_NAME = "localityName";
+	public static final String STREET = "street";
+	public static final String JPEG_PHOTO = "jpegPhoto";
+	public static final String LABELED_URL = "labeledURL";
+	public static final String STATE = "st";
+	public static final String TITLE = "title";
+	public static final String TELEPHONE_NUMBER = "telephoneNumber";
 	public static final String EMAIL = "email";
 
 	/**
