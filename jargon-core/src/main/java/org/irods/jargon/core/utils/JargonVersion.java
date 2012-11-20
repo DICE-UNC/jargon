598539a32d8fbca0e85778814bd28a34e2fd2e60
mconway <michael.c.conway@gmail.com>
2012-11-20 09:35:19 -0500

    [#1061] Jargon 3.2.1 release activities

--- jargon-core/src/main/java/org/irods/jargon/core/utils/JargonVersion.java
+++ jargon-core/src/main/java/org/irods/jargon/core/utils/JargonVersion.java
@@ -1,5 +1,5 @@
 package org.irods.jargon.core.utils;
 public final class JargonVersion {
- public static String VERSION="3.2.1.1-SNAPSHOT";
- public static String BUILD_TIME="20121120-0807";
+ public static String VERSION="3.2.1.1";
+ public static String BUILD_TIME="20121120-0928";
 }
