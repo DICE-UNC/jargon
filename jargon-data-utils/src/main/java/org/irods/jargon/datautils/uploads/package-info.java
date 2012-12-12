887aa8aad16dc2aaceda2ff0c702ee3725bc7501
mconway <michael.c.conway@gmail.com>
2012-12-12 18:52:09 -0500

    [#984] adding uploads support

---
+++ jargon-data-utils/src/main/java/org/irods/jargon/datautils/uploads/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package to help manage the 'uploads' user folder convention. This is a standard folder to which default 
+ * uploads from various interfaces will go.
+ * 
+ * @author Mike Conway - DICE (www.irods.org)
+ *
+ */
+package org.irods.jargon.datautils.uploads;
