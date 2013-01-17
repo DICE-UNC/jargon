321c70a8b1ce9fe20378fd9ae20460e493c182b8
mconway <michael.c.conway@gmail.com>
2013-01-17 13:07:08 -0500

    [#1140] add total records to user findAll result

--- jargon-core/src/main/java/org/irods/jargon/core/utils/JargonVersion.java
+++ jargon-core/src/main/java/org/irods/jargon/core/utils/JargonVersion.java
@@ -1,5 +1,5 @@
 package org.irods.jargon.core.utils;
 public final class JargonVersion {
- public static String VERSION="3.2.1.4-SNAPSHOT";
- public static String BUILD_TIME="20130117-1155";
+ public static String VERSION="3.3.0-SNAPSHOT";
+ public static String BUILD_TIME="20130117-1259";
 }
