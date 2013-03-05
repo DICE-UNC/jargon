b047ad09e16f5a40c2efa76fc9a5dde3fc0bbf72
mconway <michael.c.conway@gmail.com>
2013-03-05 11:10:10 -0500

    [#1191] set up jargon for 3.3.2 in dev

--- jargon-core/src/main/java/org/irods/jargon/core/utils/JargonVersion.java
+++ jargon-core/src/main/java/org/irods/jargon/core/utils/JargonVersion.java
@@ -1,5 +1,5 @@
 package org.irods.jargon.core.utils;
 public final class JargonVersion {
- public static String VERSION="3.3.0";
- public static String BUILD_TIME="20130304-1537";
+ public static String VERSION="3.3.2-SNAPSHOT";
+ public static String BUILD_TIME="20130305-1039";
 }
