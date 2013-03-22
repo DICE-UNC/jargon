2c9a73fce120fdc08c3b6ffe0fb2a0686cd62c1f
mconway <michael.c.conway@gmail.com>
2013-03-22 15:20:08 -0400

    [#1096] upgrade of transfer engine

--- jargon-core/src/main/java/org/irods/jargon/core/utils/JargonVersion.java
+++ jargon-core/src/main/java/org/irods/jargon/core/utils/JargonVersion.java
@@ -1,5 +1,5 @@
 package org.irods.jargon.core.utils;
 public final class JargonVersion {
  public static String VERSION="b964-SNAPSHOT";
- public static String BUILD_TIME="20130314-1024";
+ public static String BUILD_TIME="20130315-1139";
 }
