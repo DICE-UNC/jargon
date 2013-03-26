257131a9ea0417f6dc3c1af31642cdeadfd64915
mconway <michael.c.conway@gmail.com>
2013-03-26 16:24:17 -0400

    [#1182] merging in latest dev passes all tests

--- jargon-core/src/main/java/org/irods/jargon/core/pub/io/IRODSFileOutputStream.java
+++ jargon-core/src/main/java/org/irods/jargon/core/pub/io/IRODSFileOutputStream.java
@@ -83,13 +83,11 @@ public class IRODSFileOutputStream extends OutputStream {
 			JargonException {
 		int fileDescriptor = -1;
 
-		/* FIXME: need to check if 
 		if (irodsFile.exists()) {
 			log.info("deleting file, as this stream operation is overwriting");
 			irodsFile.delete();
 			irodsFile.reset();
 		}
-		*/
 
 		irodsFile.createNewFileCheckNoResourceFound();
 
