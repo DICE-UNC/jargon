b1520110a36533ff368e949d2467af47f9927750
Lisa Stillwell <lisa@renci.org>
2013-05-01 11:45:02 -0400

    [#1096] working on test for transfer accounting mgr

--- jargon-conveyor/src/test/java/org/irods/jargon/conveyor/basic/TransferAccountingManagementServiceImplTest.java
+++ jargon-conveyor/src/test/java/org/irods/jargon/conveyor/basic/TransferAccountingManagementServiceImplTest.java
@@ -1,10 +1,15 @@
 package org.irods.jargon.conveyor.basic;
 
 import java.util.Date;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.irods.jargon.conveyor.core.ConveyorExecutionException;
+import org.irods.jargon.conveyor.core.GridAccountService;
 import org.irods.jargon.conveyor.core.TransferAccountingManagementService;
+import org.irods.jargon.core.connection.IRODSAccount;
+import org.irods.jargon.testutils.TestingPropertiesHelper;
+import org.irods.jargon.transfer.dao.domain.GridAccount;
 import org.irods.jargon.transfer.dao.domain.Transfer;
 import org.irods.jargon.transfer.dao.domain.TransferType;
 import org.junit.After;
@@ -25,12 +30,19 @@ import org.springframework.transaction.annotation.Transactional;
 @TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = true)
 @Transactional
 public class TransferAccountingManagementServiceImplTest {
+        private static Properties testingProperties = new Properties();
+	private static org.irods.jargon.testutils.TestingPropertiesHelper testingPropertiesHelper = new TestingPropertiesHelper();
 
 	@Autowired
 	private TransferAccountingManagementService transferAccountingManagementService;
+        
+        @Autowired
+	private GridAccountService gridAccountService;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+            org.irods.jargon.testutils.TestingPropertiesHelper testingPropertiesLoader = new TestingPropertiesHelper();
+            testingProperties = testingPropertiesLoader.getTestProperties();
 	}
 
 	@AfterClass
@@ -46,13 +58,22 @@ public class TransferAccountingManagementServiceImplTest {
 	}
 
 	@Test
-	public void testPrepareTransferForExecution() throws Exception{
+	public void testPrepareTransferForExecution() throws Exception {
+            String testUserName = "user1";
+            IRODSAccount irodsAccount = testingPropertiesHelper
+                            .buildIRODSAccountForIRODSUserFromTestPropertiesForGivenUser(
+                                            testingProperties, testUserName, testUserName);
+            String passPhrase = "ooogabooga";
+            gridAccountService.validatePassPhrase(passPhrase);
+            GridAccount gridAccount = gridAccountService
+                            .addOrUpdateGridAccountBasedOnIRODSAccount(irodsAccount);
             
             Transfer transfer = new Transfer();
             transfer.setCreatedAt(new Date());
             transfer.setIrodsAbsolutePath("/path");
             transfer.setLocalAbsolutePath("local");
             transfer.setTransferType(TransferType.PUT);
+            transfer.setGridAccount(gridAccount);
 
             transferAccountingManagementService.prepareTransferForExecution(transfer);
 
