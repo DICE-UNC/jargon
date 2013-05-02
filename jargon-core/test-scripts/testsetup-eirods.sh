78663a6b75fee1696d845516e88b6b9f7dc5d341
mconway <michael.c.conway@gmail.com>
2013-05-02 11:16:07 -0400

    [#902] IP - ils -A performance fix

---
+++ jargon-core/test-scripts/testsetup-eirods.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+iadmin mkuser test1 rodsadmin
+
+iadmin moduser test1 password test
+
+iadmin aua test1 test1DN
+
+iadmin mkuser test2 rodsuser
+
+iadmin moduser test2 password test
+
+iadmin mkuser test3 rodsuser
+
+iadmin moduser test3 password test
+
+iadmin mkresc test1-resc "unix file system" cache localhost "/var/lib/eirods/Vault1"
+
+iadmin mkresc test1-resc2 "unix file system" cache localhost "/var/lib/eirods/Vault2"
+
+iadmin atrg testResourceGroup test1-resc2
+
+iadmin mkuser anonymous rodsuser
+
+iadmin mkgroup jargonTestUg
+
+iadmin atg jargonTestUg test1
+
+iadmin atg jargonTestUg test3
+
