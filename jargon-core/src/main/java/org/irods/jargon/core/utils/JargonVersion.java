cc52c5d4243a174be0cff47074d940505f0a117d
mconway <michael_conway@unc.edu>
2013-05-09 13:27:56 -0700

    [#1396] cross zone testing phase II

--- jargon-core/src/main/java/org/irods/jargon/core/utils/JargonVersion.java
+++ jargon-core/src/main/java/org/irods/jargon/core/utils/JargonVersion.java
@@ -1,5 +1,5 @@
 package org.irods.jargon.core.utils;
 public final class JargonVersion {
  public static String VERSION="3.3.1-SNAPSHOT";
- public static String BUILD_TIME="20130509-0841";
+ public static String BUILD_TIME="20130509-0910";
 }
