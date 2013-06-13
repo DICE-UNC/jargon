d0f848fd620f0894bc14f62d4ba759424c8fb158
mconway <michael_conway@unc.edu>
2013-06-13 08:41:46 -0400

    [#1460] fix snapshot version

--- jargon-core/src/main/java/org/irods/jargon/core/utils/JargonVersion.java
+++ jargon-core/src/main/java/org/irods/jargon/core/utils/JargonVersion.java
@@ -1,5 +1,5 @@
 package org.irods.jargon.core.utils;
 public final class JargonVersion {
- public static String VERSION="3.3.1-beta1";
- public static String BUILD_TIME="20130612-1142";
+ public static String VERSION="3.1.1-SNAPSHOT";
+ public static String BUILD_TIME="20130613-0829";
 }
