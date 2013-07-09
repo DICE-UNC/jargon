a558c59f3e0161e41f208eefd1767525bf757992
mconway <michael_conway@unc.edu>
2013-07-09 13:25:24 -0400

    [#964] work on replication

--- jargon-core/src/main/java/org/irods/jargon/core/utils/JargonVersion.java
+++ jargon-core/src/main/java/org/irods/jargon/core/utils/JargonVersion.java
@@ -1,5 +1,5 @@
 package org.irods.jargon.core.utils;
 public final class JargonVersion {
  public static String VERSION="b964-SNAPSHOT";
- public static String BUILD_TIME="20130708-1143";
+ public static String BUILD_TIME="20130709-1113";
 }
