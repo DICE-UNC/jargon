2b5c078838cc0200c98092f1f310a85ef137923a
mconway <michael_conway@unc.edu>
2013-07-26 09:47:17 -0400

    [#1603] post release setup

--- jargon-core/src/main/java/org/irods/jargon/core/utils/JargonVersion.java
+++ jargon-core/src/main/java/org/irods/jargon/core/utils/JargonVersion.java
@@ -1,5 +1,5 @@
 package org.irods.jargon.core.utils;
 public final class JargonVersion {
- public static String VERSION="3.3.1-SNAPSHOT";
- public static String BUILD_TIME="20130724-0938";
+ public static String VERSION="3.3.2-SNAPSHOT";
+ public static String BUILD_TIME="20130726-0943";
 }
