14b29fbdf7ec024d36c6aa898844d3f6bb64c5e4
mconway <michael_conway@unc.edu>
2013-08-13 09:22:52 -0400

    [#1182] data object testing

---
+++ jargon-core/src/test/java/org/irods/jargon/core/pub/MountedFilesystemDataObjectAOImplTest.java
@@ -0,0 +1,42 @@
+package org.irods.jargon.core.pub;
+
+import java.util.Properties;
+
+import org.irods.jargon.testutils.AssertionHelper;
+import org.irods.jargon.testutils.IRODSTestSetupUtilities;
+import org.irods.jargon.testutils.TestingPropertiesHelper;
+import org.irods.jargon.testutils.filemanip.ScratchFileUtils;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+public class MountedFilesystemDataObjectAOImplTest {
+
+	private static Properties testingProperties = new Properties();
+	private static TestingPropertiesHelper testingPropertiesHelper = new TestingPropertiesHelper();
+	private static ScratchFileUtils scratchFileUtils = null;
+	public static final String IRODS_TEST_SUBDIR_PATH = "MountedFilesystemDataObjectAOImplTest";
+	private static IRODSTestSetupUtilities irodsTestSetupUtilities = null;
+	private static AssertionHelper assertionHelper = null;
+	private static IRODSFileSystem irodsFileSystem;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		TestingPropertiesHelper testingPropertiesLoader = new TestingPropertiesHelper();
+		testingProperties = testingPropertiesLoader.getTestProperties();
+		scratchFileUtils = new ScratchFileUtils(testingProperties);
+		scratchFileUtils
+				.clearAndReinitializeScratchDirectory(IRODS_TEST_SUBDIR_PATH);
+		irodsTestSetupUtilities = new IRODSTestSetupUtilities();
+		irodsTestSetupUtilities.initializeIrodsScratchDirectory();
+		irodsTestSetupUtilities
+				.initializeDirectoryForTest(IRODS_TEST_SUBDIR_PATH);
+		assertionHelper = new AssertionHelper();
+		irodsFileSystem = IRODSFileSystem.instance();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		irodsFileSystem.closeAndEatExceptions();
+	}
+
+}
