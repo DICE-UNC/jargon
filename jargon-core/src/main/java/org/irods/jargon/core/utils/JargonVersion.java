9a8479c0dccccf557f5270e580a8fcf6aaf90f00
mconway <michael.c.conway@gmail.com>
2013-08-15 08:10:38 -0400

    [#964] integrate latest dev with workflow

--- jargon-core/src/main/java/org/irods/jargon/core/utils/JargonVersion.java
+++ jargon-core/src/main/java/org/irods/jargon/core/utils/JargonVersion.java
@@ -1,10 +1,5 @@
 package org.irods.jargon.core.utils;
 public final class JargonVersion {
-<<<<<<< HEAD
  public static String VERSION="b964-SNAPSHOT";
- public static String BUILD_TIME="20130808-1017";
-=======
- public static String VERSION="3.3.3-alpha1";
- public static String BUILD_TIME="20130813-1536";
->>>>>>> origin/development
+ public static String BUILD_TIME="20130815-0808";
 }
