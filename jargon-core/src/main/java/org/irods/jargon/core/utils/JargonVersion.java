2b23b25ba2e4467c9550e25b684d041d0c9a6700
mconway <michael_conway@unc.edu>
2013-08-30 11:40:58 -0400

    merge latest into dev

--- jargon-core/src/main/java/org/irods/jargon/core/utils/JargonVersion.java
+++ jargon-core/src/main/java/org/irods/jargon/core/utils/JargonVersion.java
@@ -1,5 +1,5 @@
 package org.irods.jargon.core.utils;
 public final class JargonVersion {
  public static String VERSION="b964-SNAPSHOT";
- public static String BUILD_TIME="20130821-1619";
+ public static String BUILD_TIME="20130822-1207";
 }
