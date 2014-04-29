8314e968f759429242017dab05bbd53308804600
mconway <michael_conway@unc.edu>
2014-04-29 12:30:36 -0400

    #11 added data utils archiving package to create a tar

---
+++ jargon-data-utils/src/main/java/org/irods/jargon/datautils/filearchive/LocalFileArchiveServiceImpl.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.irods.jargon.datautils.filearchive;
+
+/**
+ * Utility class to create various archive types from the local file system
+ * 
+ * @author Mike Conway - DICE
+ */
+public class LocalFileArchiveServiceImpl {
+
+	/**
+	 * 
+	 */
+	public LocalFileArchiveServiceImpl() {
+	}
+
+}
