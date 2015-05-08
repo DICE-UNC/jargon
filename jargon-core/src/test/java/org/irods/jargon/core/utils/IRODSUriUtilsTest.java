5a932c164c6160d845c7fd2e799940f65115436a
Tony Edgin <tedgin@iplantcollaborative.org>
2015-05-08 12:48:53 -0700

    Simplified IRODSUriUtilsTest so it could be run without iRODS.

--- jargon-core/src/test/java/org/irods/jargon/core/utils/IRODSUriUtilsTest.java
+++ jargon-core/src/test/java/org/irods/jargon/core/utils/IRODSUriUtilsTest.java
@@ -1,151 +1,104 @@
 package org.irods.jargon.core.utils;
 
 import java.net.URI;
-import java.util.Properties;
-
-import junit.framework.Assert;
 
 import org.irods.jargon.core.connection.IRODSAccount;
-import org.irods.jargon.core.pub.IRODSFileSystem;
-import org.irods.jargon.testutils.TestingPropertiesHelper;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class IRODSUriUtilsTest {
 
-	private static Properties testingProperties = new Properties();
-	private static org.irods.jargon.testutils.TestingPropertiesHelper testingPropertiesHelper = new TestingPropertiesHelper();
-	public static final String IRODS_TEST_SUBDIR_PATH = "IRODSUriUtilsTest";
-	private static org.irods.jargon.testutils.IRODSTestSetupUtilities irodsTestSetupUtilities = null;
-	private static IRODSFileSystem irodsFileSystem = null;
+	private final String user = "user";
+	private final String zone = "zone";
+	private final String password = "password";
+	private final String host = "host.domain";
+	private final int port = 10000;
+	private final String resource = "resource";
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		org.irods.jargon.testutils.TestingPropertiesHelper testingPropertiesLoader = new TestingPropertiesHelper();
-		testingProperties = testingPropertiesLoader.getTestProperties();
-		new org.irods.jargon.testutils.filemanip.ScratchFileUtils(
-				testingProperties);
-		irodsTestSetupUtilities = new org.irods.jargon.testutils.IRODSTestSetupUtilities();
-		irodsTestSetupUtilities.initializeIrodsScratchDirectory();
-		irodsTestSetupUtilities
-				.initializeDirectoryForTest(IRODS_TEST_SUBDIR_PATH);
-		irodsFileSystem = IRODSFileSystem.instance();
+	private String home;
+	private IRODSAccount irodsAccount;
+	private String path;
+
+	public IRODSUriUtilsTest() {
+		home = "/" + zone + "/home/" + user;
+		irodsAccount = new IRODSAccount(host, port, user, password, home, zone,
+				resource);
+		path = home + "/afile.txt";
 	}
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-		irodsFileSystem.closeAndEatExceptions();
+	private URI makeURI(final Object userInfo) throws Exception {
+		final StringBuilder uriBuilder = new StringBuilder();
+		uriBuilder.append("irods://");
+		uriBuilder.append(userInfo).append("@");
+		uriBuilder.append(host).append(":").append(port);
+		uriBuilder.append(path);
+		return new URI(uriBuilder.toString());
+	}
+
+	private URI makeFullURI() throws Exception {
+		return makeURI(user + "." + zone + ":" + password);
 	}
 
 	@Test
 	public void testGetUserNameFromURI() throws Exception {
-		IRODSAccount irodsAccount = testingPropertiesHelper
-				.buildIRODSAccountFromTestProperties(testingProperties);
-		URI testURI = testingPropertiesHelper
-				.buildUriFromTestPropertiesForFileInUserDir(testingProperties,
-						"home/afile.txt");
-		String actual = IRODSUriUtils.getUserNameFromURI(testURI);
+		final String actual = IRODSUriUtils.getUserNameFromURI(makeFullURI());
 		Assert.assertNotNull("null user name", actual);
-		Assert.assertEquals("did not derive user name from URI",
-				irodsAccount.getUserName(), actual);
+		Assert.assertEquals("did not derive user name from URI", user, actual);
 	}
 
 	@Test
 	public void testGetPasswordFromURI() throws Exception {
-		IRODSAccount irodsAccount = testingPropertiesHelper
-				.buildIRODSAccountFromTestProperties(testingProperties);
-		URI testURI = testingPropertiesHelper
-				.buildUriFromTestPropertiesForFileInUserDir(testingProperties,
-						"home/afile.txt");
-		String actual = IRODSUriUtils.getPasswordFromURI(testURI);
+		final String actual = IRODSUriUtils.getPasswordFromURI(makeFullURI());
 		Assert.assertNotNull("null password", actual);
 		Assert.assertEquals("did not derive password from URI",
-				irodsAccount.getPassword(), actual);
+				password, actual);
 	}
 
 	@Test
 	public void testGetPasswordFromURINoPassword() throws Exception {
-
-		URI testURI = testingPropertiesHelper
-				.buildUriFromTestPropertiesForFileNoUserInfo(testingProperties,
-						"home/afile.txt");
-
+		final URI testURI = makeURI(user + "." + zone);
 		String actual = IRODSUriUtils.getPasswordFromURI(testURI);
 		Assert.assertNull("password should be null", actual);
 	}
 
 	@Test
 	public void testGetZoneFromURI() throws Exception {
-		IRODSAccount irodsAccount = testingPropertiesHelper
-				.buildIRODSAccountFromTestProperties(testingProperties);
-		URI testURI = testingPropertiesHelper
-				.buildUriFromTestPropertiesForFileNoUserInfo(testingProperties,
-						"home/afile.txt");
-		String actual = IRODSUriUtils.getZoneFromURI(testURI);
+		final String actual = IRODSUriUtils.getZoneFromURI(makeFullURI());
 		Assert.assertNotNull("null zone", actual);
-		Assert.assertEquals("did not derive zone from URI",
-				irodsAccount.getZone(), actual);
+		Assert.assertEquals("did not derive zone from URI", zone, actual);
 	}
 
 	@Test
 	public void testGetHostFromURI() throws Exception {
-		IRODSAccount irodsAccount = testingPropertiesHelper
-				.buildIRODSAccountFromTestProperties(testingProperties);
-		URI testURI = testingPropertiesHelper
-				.buildUriFromTestPropertiesForFileInUserDir(testingProperties,
-						"home/afile.txt");
-		String actual = IRODSUriUtils.getHostFromURI(testURI);
+		final String actual = IRODSUriUtils.getHostFromURI(makeFullURI());
 		Assert.assertNotNull("null host", actual);
-		Assert.assertEquals("did not derive host from URI",
-				irodsAccount.getHost(), actual);
+		Assert.assertEquals("did not derive host from URI", host, actual);
 	}
 
 	@Test
 	public void testGetPortFromURI() throws Exception {
-		IRODSAccount irodsAccount = testingPropertiesHelper
-				.buildIRODSAccountFromTestProperties(testingProperties);
-		URI testURI = testingPropertiesHelper
-				.buildUriFromTestPropertiesForFileInUserDir(testingProperties,
-						"home/afile.txt");
-		int actual = IRODSUriUtils.getPortFromURI(testURI);
-		Assert.assertEquals("did not derive port from URI",
-				irodsAccount.getPort(), actual);
+		final int actual = IRODSUriUtils.getPortFromURI(makeFullURI());
+		Assert.assertEquals("did not derive port from URI", port, actual);
 	}
 
 	@Test
 	public void testGetAbsolutePathFromURI() throws Exception {
-
-		URI testURI = testingPropertiesHelper
-				.buildUriFromTestPropertiesForFileInUserDir(testingProperties,
-						"home/afile.txt");
+		final URI testURI = makeFullURI();
 		String actual = IRODSUriUtils.getAbsolutePathFromURI(testURI);
 		Assert.assertNotNull("no path returned", actual);
 	}
 
 	@Test
 	public void testGetURIFromAccountAndPath() throws Exception {
-		String targetIrodsCollection = testingPropertiesHelper
-				.buildIRODSCollectionAbsolutePathFromTestProperties(
-						testingProperties, IRODS_TEST_SUBDIR_PATH);
-		IRODSAccount irodsAccount = testingPropertiesHelper
-				.buildIRODSAccountFromTestProperties(testingProperties);
-		URI testURI = testingPropertiesHelper
-				.buildUriFromTestPropertiesForFileInUserDirNoPasswordOrZone(
-						testingProperties, IRODS_TEST_SUBDIR_PATH);
-		Assert.assertEquals(
-				"uri not computed correctly",
-				testURI.toString(),
-				IRODSUriUtils.buildURIForAnAccountAndPath(irodsAccount,
-						targetIrodsCollection).toString());
-
+		final URI testURI = makeURI(user);
+		final URI actualURI = IRODSUriUtils.buildURIForAnAccountAndPath(
+				irodsAccount, path);
+		Assert.assertEquals("uri not computed correctly", testURI, actualURI);
 	}
 
 	@Test
 	public void buildURIFromIRODSAccountAndThenBuildIRODSAccountFromThatURI()
 			throws Exception {
-		IRODSAccount irodsAccount = testingPropertiesHelper
-				.buildIRODSAccountFromTestProperties(testingProperties);
 		URI testURI = irodsAccount.toURI(true);
 		IRODSAccount newAccount = IRODSUriUtils.getIRODSAccountFromURI(testURI);
 		Assert.assertNotNull("null iRODS account", newAccount);
