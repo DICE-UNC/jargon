dbf281214e097c2b251cfa086c87737328211f0f
Tony Edgin <tedgin@iplantcollaborative.org>
2015-05-13 13:53:46 -0700

    Added URI encoding and decoding
    The  user names, zones and passwords are correctly encoded and decoded.  This allows them to contain '.' and ':' characters.
    
    Moved IRODSUriUserInfo to be and inner class of IRODSUriUtils.

--- jargon-core/src/main/java/org/irods/jargon/core/utils/IRODSUriUtils.java
+++ jargon-core/src/main/java/org/irods/jargon/core/utils/IRODSUriUtils.java
@@ -1,11 +1,17 @@
 package org.irods.jargon.core.utils;
 
+import java.io.ByteArrayOutputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.irods.jargon.core.connection.IRODSAccount;
 import org.irods.jargon.core.exception.JargonException;
 import org.irods.jargon.core.exception.JargonRuntimeException;
+
 
 /**
  * Helpful methods for parsing and dealing with IRODS URIs, also supports the
@@ -17,7 +23,243 @@ import org.irods.jargon.core.exception.JargonRuntimeException;
  */
 public class IRODSUriUtils {
 
-	private static final String PATH_SEPARATOR = "/";
+	/**
+	 * This class manages the serializing and deserializing the user info
+	 * portion of an irods URI.
+	 *
+	 * If the user, zone, or password contain a '.', ':' or @, the URI will not
+	 * be parsable.
+	 */
+	public static final class UserInfo {
+
+		private static final Set<Byte> USER_INFO_ALLOWED_CHARS;
+
+		static {
+			final byte[] allowedMarks = new byte[] {
+					'-', '_', '!', '~', '*', '\'', '(', ')', ';', '&', '=', '+',
+					'$', ','
+			};
+
+			final HashSet<Byte> chars = new HashSet<>();
+			for (byte c = '0'; c <= '9'; c++) {
+				chars.add(c);
+			}
+			for (byte c = 'a'; c <= 'z'; c++) {
+				chars.add(c);
+			}
+			for (byte c = 'A'; c <= 'Z'; c++) {
+				chars.add(c);
+			}
+			for (byte c : allowedMarks) {
+				chars.add(c);
+			}
+			USER_INFO_ALLOWED_CHARS = Collections.unmodifiableSet(chars);
+		}
+
+		/**
+		 * Creates an instance that can be used to for user authentication in a
+		 * local or remote zone.
+		 *
+		 * @param user the username
+		 * @param zone the authentication zone
+		 * @param password the password used to authenticate the user
+		 * @return It returns an instance.
+		 */
+		public static UserInfo instance(final String user, final String zone,
+										final String password) {
+			return new UserInfo(user, zone, password);
+		}
+
+		/**
+		 * Creates an instance that can be used to for user authentication in
+		 * the local zone.
+		 *
+		 * @param user the username
+		 * @param password the password used to authenticate the user
+		 * @return It returns an instance.
+		 */
+		static UserInfo localInstance(final String user, final String password)
+		{
+			return new UserInfo(user, null, password);
+		}
+
+		/**
+		 * Creates an instance that doesn't contain authentication information
+		 * for a local or remote zone.
+		 *
+		 * @param user the username
+		 * @param zone the authentication zone
+		 * @return It returns an instance.
+		 */
+		static UserInfo unauthenticatedInstance(final String user,
+												final String zone) {
+			return new UserInfo(user, zone, null);
+		}
+
+		/**
+		 * Creates an instance that doesn't contain authentication information
+		 * for the local zone.
+		 *
+		 * @param user the username
+		 * @return It returns an instance.
+		 */
+		static UserInfo unauthenticatedLocalInstance(final String user) {
+			return new UserInfo(user, null, null);
+		}
+
+		/**
+		 * Creates an instance from the serialized portion of the URI.
+		 *
+		 * @param encodedStr The serialized user info portion of an irods URI
+		 * @return It returns an instance or <code>null</code> if infoStr is
+		 * <code>null</code> or empty.
+		 */
+		static UserInfo fromString(final String encodedStr) {
+			if (encodedStr == null || encodedStr.isEmpty()) {
+				return null;
+			}
+
+			final int zIdx = encodedStr.indexOf(ZONE_INDICATOR);
+			final int pIdx = encodedStr.indexOf(PASSWORD_INDICATOR);
+
+			String encodedUser = null;
+			String encodedZone = null;
+			String encodedPassword = null;
+
+			if (zIdx < 0 && pIdx < 0) {
+			    // Only user
+				encodedUser = encodedStr;
+			} else if (zIdx < 0 && pIdx >= 0) {
+			    // User and password
+				encodedUser = encodedStr.substring(0, pIdx);
+				encodedPassword = encodedStr.substring(pIdx + 1);
+			} else if (zIdx >= 0 && pIdx <= zIdx) {
+			    // User and zone
+				encodedUser = encodedStr.substring(0, zIdx);
+				encodedZone = encodedStr.substring(zIdx + 1);
+			} else {
+				// user, zone, and password
+				encodedUser = encodedStr.substring(0, zIdx);
+				encodedZone = encodedStr.substring(zIdx + 1, pIdx);
+				encodedPassword = encodedStr.substring(pIdx + 1);
+			}
+
+			return new UserInfo(decode(encodedUser), decode(encodedZone),
+					decode(encodedPassword));
+		}
+
+		private static String emptyAsNull(final String value) {
+			return (value == null || value.isEmpty()) ? null : value;
+		}
+
+		private static String decode(final String encodedValue) {
+			if (encodedValue == null) {
+				return null;
+			}
+			final byte[] encoded = encodedValue.getBytes(
+					StandardCharsets.US_ASCII);
+			final ByteArrayOutputStream decoded = new ByteArrayOutputStream();
+			for (int i = 0; i < encoded.length; i++) {
+				if (encoded[i] == ESCAPE_INDICATOR) {
+					final int ud = Character.digit(encoded[++i], 16);
+					final int ld = Character.digit(encoded[++i], 16);
+					decoded.write((ud << 4) + ld);
+				} else {
+					decoded.write(encoded[i]);
+				}
+			}
+			return new String(decoded.toByteArray(), StandardCharsets.UTF_8);
+		}
+
+		private static String encode(final String value) {
+			final byte[] decoded = value.getBytes(StandardCharsets.UTF_8);
+			final ByteArrayOutputStream encoded = new ByteArrayOutputStream();
+			for (int i = 0; i < decoded.length; i++) {
+				final byte c = decoded[i];
+				if (USER_INFO_ALLOWED_CHARS.contains(c)) {
+					encoded.write(c);
+				} else {
+					final String hex = Integer.toHexString(c & 0xff);
+					final byte[] digits = hex.toUpperCase().getBytes(
+							StandardCharsets.US_ASCII);
+					encoded.write(ESCAPE_INDICATOR);
+					encoded.write(digits, 0, digits.length);
+				}
+			}
+			return new String(encoded.toByteArray(), StandardCharsets.US_ASCII);
+		}
+
+		private final String user;
+		private final String zone;
+		private final String password;
+
+		private UserInfo(final String user, final String zone,
+						 final String password) {
+			if (user == null || user.isEmpty()) {
+				throw new IllegalArgumentException("must provide a user name");
+			}
+
+			this.user = user;
+			this.zone = emptyAsNull(zone);
+			this.password = emptyAsNull(password);
+		}
+
+		/**
+		 * Serializes the object for inclusion in an irods URI.
+		 * @return the user info portion of an irods URI
+		 */
+		@Override
+		public String toString() {
+			final StringBuilder encoded = new StringBuilder();
+			encoded.append(encode(user));
+			if (zone != null) {
+				encoded.append(ZONE_INDICATOR).append(encode(zone));
+			}
+			if (password != null) {
+				encoded.append(PASSWORD_INDICATOR).append(encode(password));
+			}
+			return encoded.toString();
+		}
+
+		/**
+		 * @return the username
+		 */
+		public String getUserName() {
+			return user;
+		}
+
+		/**
+		 * @return the authentication zone
+		 */
+		public String getZone() {
+			return zone;
+		}
+
+		/**
+		 * @return the password used for authentication of <code>username</code>
+		 */
+		public String getPassword() {
+			return password;
+		}
+
+	}
+
+	public static final String SCHEME = "irods";
+	public static final char PATH_SEPARATOR = '/';
+
+	private static final String SCHEME_TERMINUS = "://";
+
+	private static final char ZONE_INDICATOR = '.';
+	private static final char PASSWORD_INDICATOR = ':';
+	private static final char USER_INFO_TERMINUS = '@';
+	private static final char PORT_INDICATOR = ':';
+	private static final char ESCAPE_INDICATOR = '%';
+
+
+	// TODO document
+	public static UserInfo getUserInfoFromURI(final URI irodsURI) {
+		return UserInfo.fromString(irodsURI.getRawUserInfo());
+	}
 
 	/**
 	 * Retrieve the iRODS user name from the <code>URI</code>, or
@@ -30,7 +272,7 @@ public class IRODSUriUtils {
 	 *         <code>null</code> if the user name is not present.
 	 */
 	public static String getUserNameFromURI(final URI irodsURI) {
-		final IRODSUriUserInfo info = getUserInfoFromURI(irodsURI);
+		final UserInfo info = getUserInfoFromURI(irodsURI);
 		return info == null ? null : info.getUserName();
 	}
 
@@ -39,9 +281,11 @@ public class IRODSUriUtils {
 	 * 
 	 * @param irodsURI
 	 * @return
+	 * @deprecated Use <code>getUserInfoFromURI</code>
 	 */
 	@Deprecated
-	protected static URIUserParts getURIUserPartsFromUserInfo(final URI irodsURI) {
+	protected static URIUserParts getURIUserPartsFromUserInfo(
+			final URI irodsURI) {
 		String userInfo = irodsURI.getUserInfo();
 
 		if (userInfo == null || userInfo.isEmpty()) {
@@ -119,7 +363,7 @@ public class IRODSUriUtils {
 	 *         if not available.
 	 */
 	public static String getPasswordFromURI(final URI irodsURI) {
-		final IRODSUriUserInfo info = getUserInfoFromURI(irodsURI);
+		final UserInfo info = getUserInfoFromURI(irodsURI);
 		return info == null ? null : info.getPassword();
 	}
 
@@ -132,7 +376,7 @@ public class IRODSUriUtils {
 	 *         not available.
 	 */
 	public static String getZoneFromURI(final URI irodsURI) {
-		final IRODSUriUserInfo info = getUserInfoFromURI(irodsURI);
+		final UserInfo info = getUserInfoFromURI(irodsURI);
 		return info == null ? null : info.getZone();
 	}
 
@@ -189,7 +433,7 @@ public class IRODSUriUtils {
 					"cannot derive IRODSAccount, not an iRODS uri");
 		}
 
-		final IRODSUriUserInfo info = getUserInfoFromURI(irodsURI);
+		final UserInfo info = getUserInfoFromURI(irodsURI);
 
 		if (info == null
 				|| info.getPassword() == null
@@ -218,13 +462,66 @@ public class IRODSUriUtils {
 	 *         iRODS URI scheme.
 	 */
 	public static boolean isIRODSURIScheme(final URI irodsURI) {
+		return SCHEME.equals(irodsURI.getScheme());
+	}
 
-		boolean isURI = false;
-		String uriScheme = irodsURI.getScheme();
-		if (uriScheme != null && uriScheme.equals("irods")) {
-			isURI = true;
+	private static URI buildBaseURI(final String host, final int port,
+									final UserInfo userInfo)
+	{
+		try {
+			final StringBuilder uriBuilder = new StringBuilder();
+			uriBuilder.append(SCHEME).append(SCHEME_TERMINUS);
+			if (userInfo != null) {
+				uriBuilder.append(userInfo).append(USER_INFO_TERMINUS);
+			}
+			uriBuilder.append(host).append(PORT_INDICATOR).append(port);
+			uriBuilder.append(PATH_SEPARATOR);
+			return new URI(uriBuilder.toString());
+		} catch (final URISyntaxException e) {
+			throw new JargonRuntimeException(e);
 		}
-		return isURI;
+	}
+
+	// TODO document
+	public static URI buildBaseURI(final String host, final int port,
+								   final String username) {
+		final UserInfo info = UserInfo.instance(username, null, null);
+		return buildBaseURI(host, port, info);
+	}
+
+	// TODO document
+	public static URI buildURI(final String host, final int port,
+							   final UserInfo userInfo, final String absPath) {
+		try {
+			final URI base = buildBaseURI(host, port, userInfo);
+			return base.resolve(new URI(null, absPath, null));
+		} catch (final URISyntaxException e) {
+			throw new JargonRuntimeException(e);
+		}
+	}
+
+	// TODO document
+	public static URI buildURI(final String host, final int port,
+							   final String username, final String absPath) {
+		try {
+			final URI base = buildBaseURI(host, port, username);
+			return base.resolve(new URI(null, absPath, null));
+		} catch (final URISyntaxException e) {
+			throw new JargonRuntimeException(e);
+		}
+	}
+
+	// TODO document
+	public static URI buildAnonymousURI(final String host, final int port,
+										final String absPath) {
+		if (absPath == null || absPath.isEmpty()) {
+			throw new IllegalArgumentException("null or empty path");
+		}
+		try {
+			return new URI(SCHEME, null, host, port, absPath, null, null);
+		} catch (final URISyntaxException e) {
+			throw new JargonRuntimeException(e);
+		}
 	}
 
 	/**
@@ -235,6 +532,7 @@ public class IRODSUriUtils {
 	 * @param irodsPath
 	 * @return
 	 */
+	@Deprecated
 	public static URI buildURIForAnAccountAndPath(
 			final IRODSAccount irodsAccount, final String irodsPath) {
 
@@ -247,12 +545,11 @@ public class IRODSUriUtils {
 					"null or empty irodsAbsolutePath");
 		}
 
-		final IRODSUriUserInfo info =
-				IRODSUriUserInfo.unauthenticatedLocalInstance(
+		final UserInfo info = UserInfo.unauthenticatedLocalInstance(
 						irodsAccount.getUserName());
 		final String absPath = mkPathAbs(irodsAccount.getHomeDirectory(),
 				irodsPath);
-		return buildURI(irodsAccount.getHost(), irodsAccount.getPort(), absPath,
+		return buildBaseURI(irodsAccount.getHost(), irodsAccount.getPort(), absPath,
 				info);
 	}
 
@@ -264,6 +561,7 @@ public class IRODSUriUtils {
 	 * @param irodsPath
 	 * @return
 	 */
+	@Deprecated
 	public static URI buildURIForAnAccountWithNoUserInformationIncluded(
 			final IRODSAccount irodsAccount, final String irodsPath) {
 		if (irodsAccount == null) {
@@ -277,30 +575,33 @@ public class IRODSUriUtils {
 
 		final String absPath = mkPathAbs(irodsAccount.getHomeDirectory(),
 				irodsPath);
-		return buildURI(irodsAccount.getHost(), irodsAccount.getPort(), absPath,
+		return buildBaseURI(irodsAccount.getHost(), irodsAccount.getPort(), absPath,
 				null);
 	}
 
-	static IRODSUriUserInfo getUserInfoFromURI(final URI irodsURI) {
-		return IRODSUriUserInfo.fromString(irodsURI.getUserInfo());
-	}
-
-	private static URI buildURI(final String host, final int port,
-								final String path,
-								final IRODSUriUserInfo userInfo) {
+	@Deprecated
+	private static URI buildBaseURI(final String host, final int port,
+									final String absPath,
+									final UserInfo userInfo) {
 		try {
-			final String info = userInfo == null ? null : userInfo.toString();
-			return new URI("irods", info, host, port, path, null, null);
+			final StringBuilder uriBuilder = new StringBuilder();
+			uriBuilder.append(SCHEME).append(SCHEME_TERMINUS);
+			if (userInfo != null) {
+				uriBuilder.append(userInfo).append(USER_INFO_TERMINUS);
+			}
+			uriBuilder.append(host).append(PORT_INDICATOR).append(port);
+			uriBuilder.append(absPath);
+			return new URI(uriBuilder.toString());
 		} catch (final URISyntaxException e) {
-			throw new JargonRuntimeException(
-					"there is a URI creation bug in Jargon", e);
+			throw new JargonRuntimeException(e);
 		}
 	}
 
 	// if this is a relative path, use the user home directory to fashion an
 	// absolute path
+	@Deprecated
 	private static String mkPathAbs(final String homeDir, final String path) {
-		if (path.startsWith(PATH_SEPARATOR)) {
+		if (path.charAt(0) == PATH_SEPARATOR) {
 			return path;
 		} else {
 			return homeDir + PATH_SEPARATOR + path;
@@ -314,7 +615,7 @@ public class IRODSUriUtils {
  * scheme
  * 
  * @author Mike Conway - DICE (www.irods.org)
- * 
+ * @deprecated Use <code>IRODSUriUserInfo</code>
  */
 @Deprecated
 class URIUserParts {
--- jargon-core/src/test/java/org/irods/jargon/core/utils/IRODSUriUtilsTest.java
+++ jargon-core/src/test/java/org/irods/jargon/core/utils/IRODSUriUtilsTest.java
@@ -2,113 +2,140 @@ package org.irods.jargon.core.utils;
 
 import java.net.URI;
 
-import org.irods.jargon.core.connection.IRODSAccount;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class IRODSUriUtilsTest {
 
-	private final String user = "user";
-	private final String zone = "zone";
-	private final String password = "password";
-	private final String host = "host.domain";
-	private final int port = 10000;
-	private final String resource = "resource";
-
-	private String home;
-	private IRODSAccount irodsAccount;
-	private String path;
-
-	public IRODSUriUtilsTest() {
-		home = "/" + zone + "/home/" + user;
-		irodsAccount = new IRODSAccount(host, port, user, password, home, zone,
-				resource);
-		path = home + "/afile.txt";
-	}
-
-	private URI makeURI(final Object userInfo) throws Exception {
-		final StringBuilder uriBuilder = new StringBuilder();
-		uriBuilder.append("irods://");
-		uriBuilder.append(userInfo).append("@");
-		uriBuilder.append(host).append(":").append(port);
-		uriBuilder.append(path);
-		return new URI(uriBuilder.toString());
-	}
-
-	private URI makeFullURI() throws Exception {
-		return makeURI(user + "." + zone + ":" + password);
-	}
-
 	@Test
 	public void testGetUserNameFromURI() throws Exception {
-		final String actual = IRODSUriUtils.getUserNameFromURI(makeFullURI());
+		final URI uri = new URI(
+				"irods://user.zone:password@host.domain:10000/zone/home/user/afile.txt"
+		);
+		final String actual = IRODSUriUtils.getUserNameFromURI(uri);
 		Assert.assertNotNull("null user name", actual);
-		Assert.assertEquals("did not derive user name from URI", user, actual);
+		Assert.assertEquals("did not derive user name from URI", "user",
+				actual);
 	}
 
 	@Test
 	public void testGetPasswordFromURI() throws Exception {
-		final String actual = IRODSUriUtils.getPasswordFromURI(makeFullURI());
+		final URI uri = new URI(
+				"irods://user.zone:password@host.domain:10000/zone/home/user/afile.txt"
+		);
+		final String actual = IRODSUriUtils.getPasswordFromURI(uri);
 		Assert.assertNotNull("null password", actual);
-		Assert.assertEquals("did not derive password from URI",
-				password, actual);
+		Assert.assertEquals("did not derive password from URI", "password",
+				actual);
 	}
 
 	@Test
 	public void testGetPasswordFromURINoPassword() throws Exception {
-		final URI testURI = makeURI(user + "." + zone);
+		final URI testURI = new URI(
+				"irods://user.zone@host.domain:10000/zone/home/user/afile.txt");
 		String actual = IRODSUriUtils.getPasswordFromURI(testURI);
 		Assert.assertNull("password should be null", actual);
 	}
 
 	@Test
 	public void testGetZoneFromURI() throws Exception {
-		final String actual = IRODSUriUtils.getZoneFromURI(makeFullURI());
+		final URI uri = new URI(
+				"irods://user.zone:password@host.domain:10000/zone/home/user/afile.txt"
+		);
+		final String actual = IRODSUriUtils.getZoneFromURI(uri);
 		Assert.assertNotNull("null zone", actual);
-		Assert.assertEquals("did not derive zone from URI", zone, actual);
+		Assert.assertEquals("did not derive zone from URI", "zone", actual);
 	}
 
 	@Test
 	public void testGetHostFromURI() throws Exception {
-		final String actual = IRODSUriUtils.getHostFromURI(makeFullURI());
+		final URI uri = new URI(
+				"irods://user.zone:password@host.domain:10000/zone/home/user/afile.txt"
+		);
+		final String actual = IRODSUriUtils.getHostFromURI(uri);
 		Assert.assertNotNull("null host", actual);
-		Assert.assertEquals("did not derive host from URI", host, actual);
+		Assert.assertEquals("did not derive host from URI", "host.domain",
+				actual);
 	}
 
 	@Test
 	public void testGetPortFromURI() throws Exception {
-		final int actual = IRODSUriUtils.getPortFromURI(makeFullURI());
-		Assert.assertEquals("did not derive port from URI", port, actual);
+		final URI uri = new URI(
+				"irods://user.zone:password@host.domain:10000/zone/home/user/afile.txt"
+		);
+		final int actual = IRODSUriUtils.getPortFromURI(uri);
+		Assert.assertEquals("did not derive port from URI", 10000, actual);
 	}
 
 	@Test
 	public void testGetAbsolutePathFromURI() throws Exception {
-		final URI testURI = makeFullURI();
+		final URI testURI = new URI(
+				"irods://user.zone:password@host.domain:10000/zone/home/user/afile.txt"
+		);
 		String actual = IRODSUriUtils.getAbsolutePathFromURI(testURI);
 		Assert.assertNotNull("no path returned", actual);
 	}
 
 	@Test
-	public void testGetURIFromAccountAndPath() throws Exception {
-		final URI testURI = makeURI(user);
-		final URI actualURI = IRODSUriUtils.buildURIForAnAccountAndPath(
-				irodsAccount, path);
-		Assert.assertEquals("uri not computed correctly", testURI, actualURI);
+	public void testIsIRODSURISchemeFalse() throws Exception {
+		final URI uri = new URI("http://host");
+		Assert.assertFalse(IRODSUriUtils.isIRODSURIScheme(uri));
 	}
 
 	@Test
-	public void buildURIFromIRODSAccountAndThenBuildIRODSAccountFromThatURI()
-			throws Exception {
-		URI testURI = irodsAccount.toURI(true);
-		IRODSAccount newAccount = IRODSUriUtils.getIRODSAccountFromURI(testURI);
-		Assert.assertNotNull("null iRODS account", newAccount);
-		Assert.assertEquals(irodsAccount.getHost(), newAccount.getHost());
-		Assert.assertEquals(irodsAccount.getPort(), newAccount.getPort());
-		Assert.assertEquals(irodsAccount.getZone(), newAccount.getZone());
-		Assert.assertEquals(irodsAccount.getUserName(),
-				newAccount.getUserName());
-		Assert.assertEquals(irodsAccount.getPassword(),
-				newAccount.getPassword());
+	public void testIsIRODSURISchemeTrue() throws Exception {
+		final URI uri = new URI("irods://host");
+		Assert.assertTrue(IRODSUriUtils.isIRODSURIScheme(uri));
+	}
+
+	@Test
+	public void testBuildBaseURI() throws Exception {
+		final URI expectedURI = new URI("irods://user@host.domain:10000/");
+		final URI actualURI = IRODSUriUtils.buildBaseURI("host.domain", 10000,
+				"user");
+		Assert.assertEquals("uri not computed correctly", expectedURI,
+				actualURI);
+	}
+
+	@Test
+	public void testBuildURIUserName() throws Exception {
+		final URI expectedURI = new URI(
+				"irods://user@host.domain:10000/path/to/entity");
+		final URI actualURI = IRODSUriUtils.buildURI("host.domain", 10000,
+				"user", "/path/to/entity");
+		Assert.assertEquals(expectedURI, actualURI);
+	}
+
+	@Test
+	public void testBuildURIUserInfo() throws Exception {
+		final URI expectedURI = new URI(
+				"irods://user.zone:password@host.domain:10000/path/to/entity");
+		final IRODSUriUtils.UserInfo info = IRODSUriUtils.UserInfo.instance(
+				"user", "zone", "password");
+		final URI actualURI = IRODSUriUtils.buildURI("host.domain", 10000, info,
+				"/path/to/entity");
+		Assert.assertEquals(expectedURI, actualURI);
+	}
+
+	@Test
+	public void testBuildAnonymousURI() {
+		final URI uri = IRODSUriUtils.buildAnonymousURI("host.domain", 10000,
+				"/zone/home/user/afile.txt");
+		Assert.assertNull("URI contains user info", uri.getUserInfo());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testBuildAnonymouuseURINoPath() {
+		IRODSUriUtils.buildAnonymousURI("host.domain", 10000, null);
+	}
+
+	@Test
+	public void testEcoding() throws Exception {
+		final URI expectedURI = new URI(
+				"irods://us%5ber@host.domain:10000/path/t%20o/entity%7B");
+		final URI actualURI = IRODSUriUtils.buildURI("host.domain", 10000,
+				"us[er", "/path/t o/entity{");
+		Assert.assertEquals(expectedURI, actualURI);
 	}
 
 }
