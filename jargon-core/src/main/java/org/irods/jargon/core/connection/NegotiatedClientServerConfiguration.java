29e1aed6667d7cae78f6c123ef118ff3f46b302b
mconway <michael.c.conway@gmail.com>
2015-07-16 08:06:53 -0400

    #4 add neg result

---
+++ jargon-core/src/main/java/org/irods/jargon/core/connection/NegotiatedClientServerConfiguration.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package org.irods.jargon.core.connection;
+
+/**
+ * Captures characteristics resulting from a client-server negotiation
+ * 
+ * @author Mike Conway - DICE
+ */
+public class NegotiatedClientServerConfiguration {
+
+}
