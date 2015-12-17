942e080e90d83e7853ad64f88bc9cd4ab6508699
michael-conway <michael_conway@unc.edu>
2015-12-17 07:44:00 -0500

    #165 remove old, useless thumbnail code

--- jargon-data-utils/src/test/java/org/irods/jargon/datautils/unittest/AllTests.java
+++ jargon-data-utils/src/test/java/org/irods/jargon/datautils/unittest/AllTests.java
@@ -7,9 +7,6 @@ import org.irods.jargon.datautils.datacache.DataCacheServiceImplTest;
 import org.irods.jargon.datautils.filearchive.LocalFileGzipCompressorTest;
 import org.irods.jargon.datautils.filearchive.LocalTarFileArchiverTest;
 import org.irods.jargon.datautils.filesampler.FileSamplerServiceImplTest;
-import org.irods.jargon.datautils.image.ImageServiceFactoryImplTest;
-import org.irods.jargon.datautils.image.MediaHandlingUtilsTest;
-import org.irods.jargon.datautils.image.ThumbnailServiceImplTest;
 import org.irods.jargon.datautils.sharing.AnonymousAccessServiceImplTest;
 import org.irods.jargon.datautils.shoppingcart.FileShoppingCartTest;
 import org.irods.jargon.datautils.shoppingcart.ShoppingCartEntryTest;
@@ -31,9 +28,8 @@ import org.junit.runners.Suite;
 		DataCacheServiceImplTest.class,
 		ConnectionCreatingPoolableObjectFactoryTest.class,
 		TempPasswordCachingProtocolManagerTest.class,
-		ThumbnailServiceImplTest.class, ImageServiceFactoryImplTest.class,
 		ShoppingCartEntryTest.class, FileShoppingCartTest.class,
-		MediaHandlingUtilsTest.class, ShoppingCartServiceImplTest.class,
+		ShoppingCartServiceImplTest.class,
 		AnonymousAccessServiceImplTest.class, UploadsServiceImplTest.class,
 		DiffTreePostProcessorTest.class,
 		FileTreeIteratorVisitorInvokerTest.class,
