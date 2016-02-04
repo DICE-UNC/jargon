b9104999d002bcf8e59c12265be95f64ff685b06
michael-conway <michael_conway@unc.edu>
2016-02-04 08:39:04 -0500

    #171 adding data object queries

--- jargon-mdquery/src/main/java/org/irods/jargon/mdquery/service/MetadataQueryServiceImpl.java
+++ jargon-mdquery/src/main/java/org/irods/jargon/mdquery/service/MetadataQueryServiceImpl.java
@@ -3,6 +3,7 @@
  */
 package org.irods.jargon.mdquery.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.irods.jargon.core.connection.IRODSAccount;
@@ -10,11 +11,15 @@ import org.irods.jargon.core.exception.JargonException;
 import org.irods.jargon.core.pub.IRODSAccessObjectFactory;
 import org.irods.jargon.core.pub.ListAndCount;
 import org.irods.jargon.core.query.CollectionAndDataObjectListingEntry;
+import org.irods.jargon.core.query.CollectionAndDataObjectListingEntry.ObjectType;
 import org.irods.jargon.core.query.GenQueryBuilderException;
 import org.irods.jargon.core.query.IRODSGenQueryBuilder;
 import org.irods.jargon.core.query.IRODSGenQueryFromBuilder;
+import org.irods.jargon.core.query.IRODSQueryResultRow;
 import org.irods.jargon.core.query.IRODSQueryResultSetInterface;
 import org.irods.jargon.core.query.JargonQueryException;
+import org.irods.jargon.core.query.MetaDataAndDomainData;
+import org.irods.jargon.core.query.MetaDataAndDomainData.MetadataDomain;
 import org.irods.jargon.core.query.PagingAwareCollectionListing;
 import org.irods.jargon.core.query.QueryConditionOperators;
 import org.irods.jargon.core.query.RodsGenQueryEnum;
@@ -24,6 +29,7 @@ import org.irods.jargon.mdquery.MetadataQuery;
 import org.irods.jargon.mdquery.MetadataQuery.QueryType;
 import org.irods.jargon.mdquery.MetadataQueryElement;
 import org.irods.jargon.mdquery.exception.MetadataQueryException;
+import org.irods.jargon.mdquery.serialization.MetadataQueryJsonService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -66,7 +72,11 @@ public class MetadataQueryServiceImpl extends AbstractJargonService implements
 	@Override
 	public PagingAwareCollectionListing executeQuery(final String jsonString)
 			throws MetadataQueryException {
-		return null;
+		MetadataQueryJsonService metadataQueryJsonService = new MetadataQueryJsonService();
+		MetadataQuery query = metadataQueryJsonService
+				.metadataQueryFromJson(jsonString);
+		return this.executeQuery(query);
+
 	}
 
 	/*
@@ -104,26 +114,27 @@ public class MetadataQueryServiceImpl extends AbstractJargonService implements
 				|| metadataQuery.getQueryType() == QueryType.COLLECTIONS) {
 
 			ListAndCount collections = queryCollections(metadataQuery);
-			listing.getCollectionAndDataObjectListingEntries().addAll(
-					collections.getCollectionAndDataObjectListingEntries());
+			listing.setCollectionAndDataObjectListingEntries(collections
+					.getCollectionAndDataObjectListingEntries());
 			listing.getPagingAwareCollectionListingDescriptor()
 					.setCollectionsComplete(collections.isEndOfRecords());
-
 			listing.getPagingAwareCollectionListingDescriptor().setCount(
 					collections.getCountThisPage());
 
-		} else if (metadataQuery.getQueryType() == QueryType.BOTH
+		} else {
+			listing.getPagingAwareCollectionListingDescriptor()
+					.setCollectionsComplete(true);
+		}
+
+		if (metadataQuery.getQueryType() == QueryType.BOTH
 				|| metadataQuery.getQueryType() == QueryType.DATA) {
 
 			log.info("querying data objects");
 			ListAndCount dataObjects = queryDataObjects(metadataQuery);
-			listing.getCollectionAndDataObjectListingEntries().addAll(
-					dataObjects.getCollectionAndDataObjectListingEntries());
-			listing.getPagingAwareCollectionListingDescriptor()
-					.setDataObjectsComplete(dataObjects.isEndOfRecords());
 
+		} else {
 			listing.getPagingAwareCollectionListingDescriptor()
-					.setDataObjectsCount(dataObjects.getCountThisPage());
+					.setDataObjectsComplete(true);
 
 		}
 
@@ -132,10 +143,113 @@ public class MetadataQueryServiceImpl extends AbstractJargonService implements
 
 	}
 
-	private ListAndCount queryDataObjects(MetadataQuery metadataQuery) {
+	private ListAndCount queryDataObjects(MetadataQuery metadataQuery)
+			throws MetadataQueryException {
 		log.info("queryDataObjects()");
 
-		return null;
+		log.info("queryCollections()");
+		IRODSGenQueryBuilder builder = new IRODSGenQueryBuilder(true, null);
+		IRODSQueryResultSetInterface resultSet = null;
+
+		try {
+			builder.addSelectAsGenQueryValue(RodsGenQueryEnum.COL_COLL_NAME)
+					.addSelectAsGenQueryValue(RodsGenQueryEnum.COL_DATA_NAME)
+					.addSelectAsGenQueryValue(RodsGenQueryEnum.COL_DATA_SIZE)
+					.addSelectAsGenQueryValue(
+							RodsGenQueryEnum.COL_D_CREATE_TIME)
+					.addSelectAsGenQueryValue(
+							RodsGenQueryEnum.COL_D_MODIFY_TIME)
+					.addSelectAsGenQueryValue(RodsGenQueryEnum.COL_D_OWNER_NAME)
+					.addSelectAsGenQueryValue(RodsGenQueryEnum.COL_D_OWNER_ZONE);
+		} catch (GenQueryBuilderException e) {
+			log.error("error building query for data objects:{}",
+					metadataQuery, e);
+			throw new MetadataQueryException("gen query error", e);
+		}
+
+		if (!metadataQuery.getPathHint().isEmpty()) {
+			log.info("adding path hint for :{}", metadataQuery.getPathHint());
+			builder.addConditionAsGenQueryField(RodsGenQueryEnum.COL_COLL_NAME,
+					QueryConditionOperators.EQUAL, metadataQuery.getPathHint());
+		}
+
+		/**
+		 * Add an AVU query for each element
+		 */
+		for (MetadataQueryElement element : metadataQuery
+				.getMetadataQueryElements()) {
+			log.info("element:{}", element);
+
+			builder.addConditionAsGenQueryField(
+					RodsGenQueryEnum.COL_META_DATA_ATTR_NAME,
+					QueryConditionOperators.EQUAL, element.getAttributeName()
+							.trim());
+
+			if (element.getAttributeValue().size() > 1) {
+				throw new UnsupportedOperationException(
+						"in and between not coded yet");
+			}
+
+			if (element.getAttributeValue().size() == 1) {
+				log.info("single value operation");
+				builder.addConditionAsGenQueryField(
+						RodsGenQueryEnum.COL_META_DATA_ATTR_VALUE,
+						QueryConditionOperators
+								.getOperatorFromStringValue(element
+										.getOperator().getOperatorValue()),
+						element.getAttributeValue().get(0).trim());
+			}
+
+		}
+
+		IRODSGenQueryFromBuilder irodsQuery;
+		try {
+			irodsQuery = builder.exportIRODSQueryFromBuilder(this
+					.getIrodsAccessObjectFactory().getJargonProperties()
+					.getMaxFilesAndDirsQueryMax());
+			String targetZone = deriveTargetZone(metadataQuery);
+
+			resultSet = this
+					.getIrodsAccessObjectFactory()
+					.getIRODSGenQueryExecutor(getIrodsAccount())
+					.executeIRODSQueryAndCloseResultInZone(irodsQuery, 0,
+							targetZone);
+
+			List<CollectionAndDataObjectListingEntry> entries = buildListFromQueryResult(
+					MetadataDomain.DATA, resultSet);
+			ListAndCount listAndCount = new ListAndCount();
+			listAndCount.setCollectionAndDataObjectListingEntries(entries);
+
+			/*
+			 * see if the query had total records, if it did not, do a separate
+			 * query to establish total records
+			 */
+
+			if (listAndCount.getCollectionAndDataObjectListingEntries()
+					.isEmpty()) {
+				listAndCount.setCountTotal(0);
+				log.info("empty results returned");
+				listAndCount.setEndOfRecords(true);
+				return listAndCount;
+			}
+
+			int lastEntryIdx = listAndCount
+					.getCollectionAndDataObjectListingEntries().size() - 1;
+			CollectionAndDataObjectListingEntry lastEntry = listAndCount
+					.getCollectionAndDataObjectListingEntries().get(
+							lastEntryIdx);
+			listAndCount.setCountThisPage(lastEntry.getCount());
+			listAndCount.setEndOfRecords(lastEntry.isLastResult());
+			listAndCount.setOffsetStart(listAndCount
+					.getCollectionAndDataObjectListingEntries().get(0)
+					.getCount());
+			return listAndCount;
+
+		} catch (GenQueryBuilderException | JargonException
+				| JargonQueryException e) {
+			log.error("error in query for collections:{}", metadataQuery, e);
+			throw new MetadataQueryException("gen query error", e);
+		}
 	}
 
 	private ListAndCount queryCollections(MetadataQuery metadataQuery)
@@ -189,9 +303,9 @@ public class MetadataQueryServiceImpl extends AbstractJargonService implements
 				log.info("single value operation");
 				builder.addConditionAsGenQueryField(
 						RodsGenQueryEnum.COL_META_COLL_ATTR_VALUE,
-						QueryConditionOperators.EQUAL, // FIXME: hard code for
-														// equal! figure out a
-														// translation
+						QueryConditionOperators
+								.getOperatorFromStringValue(element
+										.getOperator().getOperatorValue()),
 						element.getAttributeValue().get(0).trim());
 			}
 
@@ -209,13 +323,100 @@ public class MetadataQueryServiceImpl extends AbstractJargonService implements
 					.getIRODSGenQueryExecutor(getIrodsAccount())
 					.executeIRODSQueryAndCloseResultInZone(irodsQuery, 0,
 							targetZone);
+
+			List<CollectionAndDataObjectListingEntry> entries = buildListFromQueryResult(
+					MetadataDomain.COLLECTION, resultSet);
+			ListAndCount listAndCount = new ListAndCount();
+			listAndCount.setCollectionAndDataObjectListingEntries(entries);
+
+			/*
+			 * see if the query had total records, if it did not, do a separate
+			 * query to establish total records
+			 */
+
+			if (listAndCount.getCollectionAndDataObjectListingEntries()
+					.isEmpty()) {
+				listAndCount.setCountTotal(0);
+				log.info("empty results returned");
+				listAndCount.setEndOfRecords(true);
+				return listAndCount;
+			}
+
+			int lastEntryIdx = listAndCount
+					.getCollectionAndDataObjectListingEntries().size() - 1;
+			CollectionAndDataObjectListingEntry lastEntry = listAndCount
+					.getCollectionAndDataObjectListingEntries().get(
+							lastEntryIdx);
+			listAndCount.setCountThisPage(lastEntry.getCount());
+			listAndCount.setEndOfRecords(lastEntry.isLastResult());
+			listAndCount.setOffsetStart(listAndCount
+					.getCollectionAndDataObjectListingEntries().get(0)
+					.getCount());
+			return listAndCount;
+
 		} catch (GenQueryBuilderException | JargonException
 				| JargonQueryException e) {
 			log.error("error in query for collections:{}", metadataQuery, e);
 			throw new MetadataQueryException("gen query error", e);
 		}
 
-		return null;
+	}
+
+	private static List<CollectionAndDataObjectListingEntry> buildListFromQueryResult(
+			final MetadataDomain metaDataDomain,
+			final IRODSQueryResultSetInterface irodsQueryResultSet)
+			throws JargonException {
+		if (metaDataDomain == null) {
+			throw new JargonException("null metaDataDomain");
+		}
+
+		if (irodsQueryResultSet == null) {
+			throw new JargonException("null irodsQueryResultSet");
+		}
+
+		List<CollectionAndDataObjectListingEntry> metaDataResults = new ArrayList<CollectionAndDataObjectListingEntry>();
+		for (IRODSQueryResultRow row : irodsQueryResultSet.getResults()) {
+			metaDataResults.add(buildListingFromResultSetRow(metaDataDomain,
+					row, irodsQueryResultSet.getTotalRecords()));
+		}
+
+		return metaDataResults;
+	}
+
+	private static CollectionAndDataObjectListingEntry buildListingFromResultSetRow(
+			final MetaDataAndDomainData.MetadataDomain metadataDomain,
+			final IRODSQueryResultRow row, final int totalRecordCount)
+			throws JargonException {
+
+		CollectionAndDataObjectListingEntry entry = new CollectionAndDataObjectListingEntry();
+		entry.setCount(row.getRecordCount());
+		entry.setLastResult(row.isLastResult());
+		entry.setTotalRecords(totalRecordCount);
+
+		if (metadataDomain == MetadataDomain.COLLECTION) {
+			entry.setCount(row.getRecordCount());
+			entry.setObjectType(ObjectType.COLLECTION);
+			entry.setParentPath(row.getColumn(0));
+			entry.setPathOrName(row.getColumn(1));
+			entry.setCreatedAt(row.getColumnAsDateOrNull(2));
+			entry.setModifiedAt(row.getColumnAsDateOrNull(3));
+			entry.setOwnerName(row.getColumn(4));
+			entry.setOwnerZone(row.getColumn(5));
+
+		} else {
+
+			entry.setCount(row.getRecordCount());
+			entry.setObjectType(ObjectType.DATA_OBJECT);
+			entry.setParentPath(row.getColumn(0));
+			entry.setPathOrName(row.getColumn(1));
+			entry.setDataSize(row.getColumnAsLongOrZero(2));
+			entry.setCreatedAt(row.getColumnAsDateOrNull(3));
+			entry.setModifiedAt(row.getColumnAsDateOrNull(4));
+			entry.setOwnerName(row.getColumn(5));
+			entry.setOwnerZone(row.getColumn(6));
+		}
+
+		return entry;
 	}
 
 	private String deriveTargetZone(MetadataQuery metadataQuery) {
--- jargon-mdquery/src/test/java/org/irods/jargon/mdquery/service/MetadataQueryServiceImplTest.java
+++ jargon-mdquery/src/test/java/org/irods/jargon/mdquery/service/MetadataQueryServiceImplTest.java
@@ -1,18 +1,31 @@
 package org.irods.jargon.mdquery.service;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
+
+import junit.framework.Assert;
 
 import org.irods.jargon.core.connection.IRODSAccount;
 import org.irods.jargon.core.pub.CollectionAO;
+import org.irods.jargon.core.pub.DataObjectAO;
+import org.irods.jargon.core.pub.DataTransferOperations;
 import org.irods.jargon.core.pub.IRODSAccessObjectFactory;
 import org.irods.jargon.core.pub.IRODSFileSystem;
 import org.irods.jargon.core.pub.domain.AvuData;
+import org.irods.jargon.core.pub.domain.DataObject;
 import org.irods.jargon.core.pub.io.IRODSFile;
+import org.irods.jargon.core.query.AVUQueryElement;
+import org.irods.jargon.core.query.AVUQueryElement.AVUQueryPart;
 import org.irods.jargon.core.query.AVUQueryOperatorEnum;
+import org.irods.jargon.core.query.PagingAwareCollectionListing;
 import org.irods.jargon.mdquery.MetadataQuery;
-import org.irods.jargon.mdquery.MetadataQueryElement;
 import org.irods.jargon.mdquery.MetadataQuery.QueryType;
+import org.irods.jargon.mdquery.MetadataQueryElement;
+import org.irods.jargon.mdquery.serialization.MetadataQueryJsonService;
 import org.irods.jargon.testutils.TestingPropertiesHelper;
+import org.irods.jargon.testutils.filemanip.FileGenerator;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -53,9 +66,9 @@ public class MetadataQueryServiceImplTest {
 								+ testDirName);
 
 		// initialize the AVU data
-		String expectedAttribName = "testSimpleAvuQueryNoPathHintattrib1";
-		String expectedAttribValue = "testSimpleAvuQueryNoPathHintvalue1";
-		String expectedAttribUnits = "FindDomainByMetadataQuerytest1units";
+		final String expectedAttribName = "testSimpleAvuQueryNoPathHintattrib1";
+		final String expectedAttribValue = "testSimpleAvuQueryNoPathHintvalue1";
+		final String expectedAttribUnits = "FindDomainByMetadataQuerytest1units";
 
 		IRODSAccount irodsAccount = testingPropertiesHelper
 				.buildIRODSAccountFromTestProperties(testingProperties);
@@ -82,9 +95,197 @@ public class MetadataQueryServiceImplTest {
 		MetadataQueryElement element = new MetadataQueryElement();
 		element.setAttributeName(expectedAttribName);
 		element.setOperator(AVUQueryOperatorEnum.EQUAL);
-		element.setAttributeValue(value);
+		@SuppressWarnings("serial")
+		List<String> vals = new ArrayList<String>() {
+			{
+				add(expectedAttribValue);
+			}
+		};
+		element.setAttributeValue(vals);
+
 		metadataQuery.setQueryType(QueryType.COLLECTIONS);
-		metadataQuery.getMetadataQueryElements().add(new MetadataQueryElement())
-		
+		metadataQuery.getMetadataQueryElements().add(element);
+
+		PagingAwareCollectionListing actual = metadataQueryService
+				.executeQuery(metadataQuery);
+		Assert.assertNotNull("null listing returned", actual);
+		Assert.assertEquals("no result row", 1, actual
+				.getCollectionAndDataObjectListingEntries().size());
+		Assert.assertEquals("unexpected collection",
+				testFile.getAbsolutePath(), actual
+						.getCollectionAndDataObjectListingEntries().get(0)
+						.getFormattedAbsolutePath());
+		Assert.assertEquals("incorrect collection count", 1, actual
+				.getPagingAwareCollectionListingDescriptor().getCount());
+		Assert.assertTrue("should reflect end of colls", actual
+				.getPagingAwareCollectionListingDescriptor()
+				.isCollectionsComplete());
+		Assert.assertTrue("should show data objs complete", actual
+				.getPagingAwareCollectionListingDescriptor()
+				.isDataObjectsComplete());
+
 	}
+
+	@Test
+	public void testSimpleAvuQueryAsJsonNoPathHint() throws Exception {
+		String testDirName = "testSimpleAvuQueryAsJsonNoPathHint";
+		String targetIrodsCollection = testingPropertiesHelper
+				.buildIRODSCollectionAbsolutePathFromTestProperties(
+						testingProperties, IRODS_TEST_SUBDIR_PATH + '/'
+								+ testDirName);
+
+		// initialize the AVU data
+		final String expectedAttribName = "testSimpleAvuQueryAsJsonNoPathHintattrib1";
+		final String expectedAttribValue = "testSimpleAvuQueryAsJsonNoPathHintvalue1";
+		final String expectedAttribUnits = "testSimpleAvuQueryAsJsonNoPathHintunits";
+
+		IRODSAccount irodsAccount = testingPropertiesHelper
+				.buildIRODSAccountFromTestProperties(testingProperties);
+		IRODSAccessObjectFactory accessObjectFactory = irodsFileSystem
+				.getIRODSAccessObjectFactory();
+		CollectionAO collectionAO = accessObjectFactory
+				.getCollectionAO(irodsAccount);
+
+		IRODSFile testFile = irodsFileSystem.getIRODSFileFactory(irodsAccount)
+				.instanceIRODSFile(targetIrodsCollection);
+		testFile.deleteWithForceOption();
+		testFile.mkdirs();
+
+		AvuData avuData = AvuData.instance(expectedAttribName,
+				expectedAttribValue, expectedAttribUnits);
+
+		collectionAO.deleteAVUMetadata(targetIrodsCollection, avuData);
+		collectionAO.addAVUMetadata(targetIrodsCollection, avuData);
+
+		MetadataQueryService metadataQueryService = new MetadataQueryServiceImpl(
+				accessObjectFactory, irodsAccount);
+
+		MetadataQuery metadataQuery = new MetadataQuery();
+		MetadataQueryElement element = new MetadataQueryElement();
+		element.setAttributeName(expectedAttribName);
+		element.setOperator(AVUQueryOperatorEnum.EQUAL);
+		List<String> vals = new ArrayList<String>() {
+			{
+				add(expectedAttribValue);
+			}
+		};
+		element.setAttributeValue(vals);
+
+		metadataQuery.setQueryType(QueryType.COLLECTIONS);
+		metadataQuery.getMetadataQueryElements().add(element);
+		MetadataQueryJsonService jsonService = new MetadataQueryJsonService();
+		String metadataQueryAsString = jsonService
+				.jsonFromMetadataQuery(metadataQuery);
+		System.out.println("query string:" + metadataQueryAsString);
+
+		PagingAwareCollectionListing actual = metadataQueryService
+				.executeQuery(metadataQueryAsString);
+		Assert.assertNotNull("null listing returned", actual);
+
+	}
+
+	@Test
+	public void testSimpleAvuQueryOneDataObjectNoPathHint() throws Exception {
+		String testCollName = "testSimpleAvuQueryOneDataObjectNoPathHint";
+		String testFilePrefix = "testSimpleAvuQueryOneDataObjectNoPathHint-";
+		String testFileSuffix = ".txt";
+		int count = 200;
+		String expectedAttribName = "testSimpleAvuQueryOneDataObjectNoPathHintattrib1";
+		final String expectedAttribValue = "testSimpleAvuQueryOneDataObjectNoPathHintvalue1";
+
+		IRODSAccount irodsAccount = testingPropertiesHelper
+				.buildIRODSAccountFromTestProperties(testingProperties);
+
+		String targetIrodsCollection = testingPropertiesHelper
+				.buildIRODSCollectionAbsolutePathFromTestProperties(
+						testingProperties, IRODS_TEST_SUBDIR_PATH + "/"
+								+ testCollName);
+
+		IRODSAccessObjectFactory accessObjectFactory = irodsFileSystem
+				.getIRODSAccessObjectFactory();
+
+		// generate some test files, first delete the test subdir
+
+		IRODSFile testSubdir = irodsFileSystem
+				.getIRODSFileFactory(irodsAccount).instanceIRODSFile(
+						targetIrodsCollection);
+		testSubdir.deleteWithForceOption();
+		testSubdir.mkdirs();
+
+		DataObjectAO dAO = accessObjectFactory.getDataObjectAO(irodsAccount);
+		DataTransferOperations dto = irodsFileSystem
+				.getIRODSAccessObjectFactory().getDataTransferOperations(
+						irodsAccount);
+
+		AvuData avuData = null;
+
+		String absPath = scratchFileUtils
+				.createAndReturnAbsoluteScratchPath(IRODS_TEST_SUBDIR_PATH);
+		String sourceFileAbsolutePath = FileGenerator
+				.generateFileOfFixedLengthGivenName(absPath,
+						"testFileForAVU.txt", 1);
+		File sourceFile = new File(sourceFileAbsolutePath);
+
+		IRODSFile dataFile = null;
+		StringBuilder sb = null;
+		for (int i = 0; i < count; i++) {
+			sb = new StringBuilder();
+			sb.append(testFilePrefix);
+			sb.append(i);
+			sb.append(testFileSuffix);
+			dataFile = irodsFileSystem.getIRODSFileFactory(irodsAccount)
+					.instanceIRODSFile(testSubdir.getAbsolutePath(),
+							sb.toString());
+			dto.putOperation(sourceFile, dataFile, null, null);
+			avuData = AvuData.instance(expectedAttribName, expectedAttribValue
+					+ i, "");
+			dAO.addAVUMetadata(dataFile.getAbsolutePath(), avuData);
+
+		}
+
+		ArrayList<AVUQueryElement> avus = new ArrayList<AVUQueryElement>();
+		avus.add(AVUQueryElement.instanceForValueQuery(AVUQueryPart.ATTRIBUTE,
+				AVUQueryOperatorEnum.EQUAL, expectedAttribName));
+		avus.add(AVUQueryElement.instanceForValueQuery(AVUQueryPart.VALUE,
+				AVUQueryOperatorEnum.LIKE, expectedAttribValue + "%"));
+
+		List<DataObject> files = dAO.findDomainByMetadataQuery(avus);
+		Assert.assertNotNull("null files returned", files);
+		Assert.assertTrue("did not get all of the files", files.size() >= count);
+
+		MetadataQueryService metadataQueryService = new MetadataQueryServiceImpl(
+				accessObjectFactory, irodsAccount);
+
+		MetadataQuery metadataQuery = new MetadataQuery();
+		MetadataQueryElement element = new MetadataQueryElement();
+		element.setAttributeName(expectedAttribName);
+		element.setOperator(AVUQueryOperatorEnum.EQUAL);
+		@SuppressWarnings("serial")
+		List<String> vals = new ArrayList<String>() {
+			{
+				add(expectedAttribValue);
+			}
+		};
+		element.setAttributeValue(vals);
+
+		metadataQuery.setQueryType(QueryType.DATA);
+		metadataQuery.getMetadataQueryElements().add(element);
+
+		PagingAwareCollectionListing actual = metadataQueryService
+				.executeQuery(metadataQuery);
+		Assert.assertNotNull("null listing returned", actual);
+		Assert.assertEquals("no result row", count, actual
+				.getCollectionAndDataObjectListingEntries().size());
+		Assert.assertEquals("incorrect count count", count, actual
+				.getPagingAwareCollectionListingDescriptor()
+				.getDataObjectsCount());
+		Assert.assertTrue("should reflect end of colls", actual
+				.getPagingAwareCollectionListingDescriptor()
+				.isDataObjectsComplete());
+		Assert.assertTrue("should show colls complete", actual
+				.getPagingAwareCollectionListingDescriptor()
+				.isCollectionsComplete());
+
+	}
+
 }
