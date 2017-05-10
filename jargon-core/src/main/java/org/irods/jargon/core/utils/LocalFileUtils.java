fc9f690647aa665ae40388fb7719723f7582ab4b
michael-conway <michael.c.conway@gmail.com>
2017-05-10 13:50:26 -0400

    #247 json round trip

--- jargon-core/src/main/java/org/irods/jargon/core/utils/LocalFileUtils.java
+++ jargon-core/src/main/java/org/irods/jargon/core/utils/LocalFileUtils.java
@@ -12,6 +12,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -41,6 +42,27 @@ public class LocalFileUtils {
 	 * private constructor, this is not meant to be an instantiated class.
 	 */
 	private LocalFileUtils() {
+
+	}
+
+	/**
+	 * Write the string to a file
+	 * 
+	 * @param file
+	 * @param string
+	 * @throws IOException
+	 */
+	public static void stringToFile(final File file, final String string) throws IOException {
+		if (file == null) {
+			throw new IllegalArgumentException("null file");
+		}
+
+		if (string == null || string.isEmpty()) {
+			throw new IllegalArgumentException("null or empty string");
+		}
+
+		final PrintWriter out = new PrintWriter(file.getAbsolutePath());
+		out.println(string);
 
 	}
 
