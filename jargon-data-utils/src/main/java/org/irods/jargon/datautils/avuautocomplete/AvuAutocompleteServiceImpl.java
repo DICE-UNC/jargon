dcfb3e85c1cff40a7e0bfd9d27ba66cd51321d3d
Deep Patel <deep.patel@nih.gov>
2018-04-09 15:31:25 -0400

    #292

--- jargon-data-utils/src/main/java/org/irods/jargon/datautils/avuautocomplete/AvuAutocompleteServiceImpl.java
+++ jargon-data-utils/src/main/java/org/irods/jargon/datautils/avuautocomplete/AvuAutocompleteServiceImpl.java
@@ -7,13 +7,19 @@ import org.irods.jargon.core.connection.IRODSAccount;
 import org.irods.jargon.core.exception.JargonException;
 import org.irods.jargon.core.pub.IRODSAccessObjectFactory;
 import org.irods.jargon.core.service.AbstractJargonService;
+import org.irods.jargon.datautils.metadatamanifest.MetadataManifestProcessorImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author Mike Conway - NIEHS
  *
  */
 public class AvuAutocompleteServiceImpl extends AbstractJargonService implements AvuAutocompleteService {
-
+	
+	public static final Logger log = LoggerFactory
+			.getLogger(AvuAutocompleteServiceImpl.class);
+	
 	/**
 	 * @param irodsAccessObjectFactory
 	 * @param irodsAccount
@@ -32,6 +38,24 @@ public class AvuAutocompleteServiceImpl extends AbstractJargonService implements
 	@Override
 	public AvuSearchResult gatherAvailableAttributes(final String prefix, final int offset,
 			final AvuTypeEnum avuTypeEnum) throws JargonException {
+		
+		log.info("gatherAvailableAttributes()");
+		
+		if (prefix == null || prefix.isEmpty()) {
+			throw new IllegalArgumentException("null prefix");
+		}
+		
+		if (offset < 0) {
+			throw new IllegalArgumentException("offset must be >= 0");
+		}
+		
+		if (avuTypeEnum == null) {
+			throw new IllegalArgumentException("null avuTypeEnum");
+		}
+		log.info("prefix:{}", prefix);
+		log.info("offset:{}", offset);
+		log.info("avuTypeEnum:{}", avuTypeEnum);
+		
 		return null;
 	}
 
