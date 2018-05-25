4b643306d6e9e05c764d90d896089d5727781054
Mike Conway <mike.conway@nih.gov>
2018-05-25 13:01:00 -0400

    #303

---
+++ jargon-data-utils/src/test/java/org/irods/jargon/datautils/unittest/FunctionalTests.java
@@ -0,0 +1,11 @@
+package org.irods.jargon.datautils.unittest;
+
+import org.irods.jargon.datautils.indexer.IndexerFunctionalTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ IndexerFunctionalTest.class })
+public class FunctionalTests {
+
+}
