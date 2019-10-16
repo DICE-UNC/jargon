1aa6d0c60baa1cf8e0bf180265e8979e956feeb7
Mike Conway <mike.conway@nih.gov>
2019-10-16 13:30:46 -0400

    #61 clean up fed tests

--- jargon-core/test-scripts/fedtestsetup_fedzone1.sh
+++ jargon-core/test-scripts/fedtestsetup_fedzone1.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
-iadmin mkzone fedZone2 remote fedZone2:1247
+iadmin mkzone zone2 remote zone2:1247
 
-iadmin mkuser rods#fedZone2 rodsuser
+iadmin mkuser rods#zone2 rodsuser
 
-iadmin mkuser test1#fedZone2 rodsuser
+iadmin mkuser test1#zone2 rodsuser
 
-imkdir /fedZone1/home/test1/fedread
+imkdir /zone1/home/test1/fedread
 
-imkdir /fedZone1/home/test1/fedwrite
+imkdir /zone1/home/test1/fedwrite
 
-ichmod -r inherit /fedZone1/home/test1/fedread
+ichmod -r inherit /zone1/home/test1/fedread
 
-ichmod -r inherit /fedZone1/home/test1/fedwrite
+ichmod -r inherit /zone1/home/test1/fedwrite
 
-ichmod  read test1#fedZone2 /fedZone1/home/test1/fedread
+ichmod  read test1#zone2 /zone1/home/test1/fedread
 
-ichmod  write test1#fedZone2 /fedZone1/home/test1/fedwrite	
+ichmod  write test1#zone2 /zone1/home/test1/fedwrite	
 
 
