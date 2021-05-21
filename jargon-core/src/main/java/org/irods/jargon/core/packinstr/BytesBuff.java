49def2102cc2c389dd596c11bfe8195ef9b62664
Mike Conway <mike.conway@nih.gov>
2021-05-21 08:57:47 -0400

    #382 add bytes buff

---
+++ jargon-core/src/main/java/org/irods/jargon/core/packinstr/BytesBuff.java
@@ -0,0 +1,40 @@
+/**
+ *
+ */
+package org.irods.jargon.core.packinstr;
+
+import org.irods.jargon.core.exception.JargonException;
+
+/**
+ * Translation of a BytesBuf_PI into XML protocol format.This is used for
+ * pluggable API calls
+ * 
+ * #define BytesBuf_PI "int buflen; char *buf(buflen);"
+ *
+ * @author Mike Conway - DICE (www.irods.org)
+ *
+ */
+public class BytesBuff extends AbstractIRODSPackingInstruction {
+
+	public static final String PI_TAG = "BytesBuf_PI";
+
+	private final String buffer;
+
+	public static final BytesBuff instance(final String buffer) throws JargonException {
+		return new BytesBuff(buffer);
+	}
+
+	private BytesBuff(final String buffer) throws JargonException {
+		super();
+		if (buffer == null) {
+			throw new JargonException("buffer is null");
+		}
+		this.buffer = buffer;
+	}
+
+	@Override
+	public Tag getTagValue() throws JargonException {
+		Tag startupPacket = new Tag(PI_TAG, new Tag[] { new Tag("buflen", buffer.length()), new Tag("buf", buffer), });
+		return startupPacket;
+	}
+}
