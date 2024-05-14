9b48aaa978f686e3182e296a2944c65270846b78
Kory Draughn <korydraughn@ymail.com>
2024-05-14 13:34:58 -0400

    [#446] Fix launch of iRODS consumer in testing framework.

--- docker-test-framework/4-3/start_provider.sh
+++ docker-test-framework/4-3/start_provider.sh
@@ -12,15 +12,11 @@ echo Postgres took approximately $counter seconds to fully start ...
 
 # Set up iRODS.
 python3 /var/lib/irods/scripts/setup_irods.py < /var/lib/irods/packaging/localhost_setup_postgres.input
-service irods start
+su - irods -c './irodsctl -v start'
 
 echo Running Test Setup Script
 su irods -c '/testsetup-consortium.sh'
 echo Completed Test Setup Script
 
 # Keep container running if the test fails.
-tail -f /dev/null
-# Is this better? sleep 2147483647d
-
-© 2020 GitHub, Inc.
-
+sleep 2147483647d
